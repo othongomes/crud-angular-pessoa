500de2a6583a5b0b30cdea62e725a44f5c6c81ad
Othon <othonmarcello@hotmail.com>
2024-08-14 11:24:14 -0300

    Ajuste da variável data para o back-end

--- src/app/persons/persons-form/persons-form.component.ts
+++ src/app/persons/persons-form/persons-form.component.ts
@@ -5,46 +5,49 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { AppMaterialModule } from '../../shared/app-material/app-material.module';
 import { DateFormatModule } from '../../shared/date-format/date-format.module';
 import { PersonsService } from '../services/persons.service';
+import moment from 'moment';
 
 @Component({
   selector: 'app-persons-form',
   standalone: true,
   imports: [ReactiveFormsModule, AppMaterialModule, DateFormatModule],
   templateUrl: './persons-form.component.html',
-  styleUrl: './persons-form.component.scss'
+  styleUrl: './persons-form.component.scss',
 })
 export class PersonsFormComponent {
-
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
-  private service: PersonsService, //2
-  private snackBar: MatSnackBar //5
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: PersonsService, //2
+    private snackBar: MatSnackBar //5
   ) {
     this.form = this.formBuilder.group({
-      _id: [null],
-      genero: [null],
-      nome: [null],
-      nascimento: [null],
-      email: [null],
-      cpf: [null],
+      _id: [],
+      genero: [],
+      nome: [],
+      nascimento: [],
+      email: [],
+      cpf: [],
     });
   }
 
   onSubmit() {
     //console.log(this.form.value); //1
     //this.service.save(this.form.value); //3
-    this.service.save(this.form.value)
-    .subscribe(result => console.log(result), //4
-    error => this.snackError()); //5 //6
+    if (this.form.value){
+      this.form.value.nascimento = moment(this.form.value.nascimento).format('YYYY-MM-DD');
+    }
+    this.service.save(this.form.value).subscribe(
+      (result) => console.log(result), //4
+      (error) => this.snackError()
+    ); //5 //6
   }
 
-  onCancel() {
+  onCancel() {}
 
+  snackError() {
+    //6
+    this.snackBar.open('Erro ao salvar usuário!', '', { duration: 3000 });
   }
-
-  snackError() { //6
-    this.snackBar.open('Erro ao salvar usuário!','',{duration: 3000})
-  }
-
 }
--- src/app/shared/date-format/date-format.module.ts
+++ src/app/shared/date-format/date-format.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
 
 export const MY_DATE_FORMATS = {
   parse: {
@@ -9,9 +10,9 @@ export const MY_DATE_FORMATS = {
   },
   display: {
     dateInput: 'DD/MM/YYYY',
-    monthYearLabel: 'MM YYYY',
+    monthYearLabel: 'MMM YYYY',
     dateA11yLabel: 'DD/MM/YYYY',
-    monthYearA11yLabel: 'MM YYYY',
+    monthYearA11yLabel: 'MMMM YYYY',
   },
 };
 
@@ -23,8 +24,9 @@ export const MY_DATE_FORMATS = {
     MatNativeDateModule
   ],
   providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: DateAdapter, useClass: MomentDateAdapter},
     { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
   ]
 
 })
